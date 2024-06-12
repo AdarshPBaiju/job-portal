f0fa2291d903cd0d4dbb4c338ece08a98c7dbcc8
Adarsh P Baiju <adarshpbaiju2060@gmail.com>
2024-06-12 13:13:46 +0530

    add user detail and company detail page ui

--- job_portal/admin_panel/urls.py
+++ job_portal/admin_panel/urls.py
@@ -7,5 +7,7 @@ app_name = 'admin_panel'
 urlpatterns = [
     path("", IndexView.as_view(), name="home"),
     path("users/", UsersListView.as_view(), name="userslist"),
+    path("users/Detail/", UserDetailView.as_view(), name="userdetail"),
     path("companies/", CompaniesListView.as_view(), name="companylist"),
+    path("companies/detail/", CompaniesDetailView.as_view(), name="companydetail"),
 ]
--- job_portal/admin_panel/views.py
+++ job_portal/admin_panel/views.py
@@ -8,5 +8,11 @@ class IndexView(TemplateView):
 class UsersListView(TemplateView):
     template_name = 'admin_panel/userslist.html'
 
+class UserDetailView(TemplateView):
+    template_name = 'admin_panel/User-detail.html'
+
 class CompaniesListView(TemplateView):
     template_name = 'admin_panel/companieslist.html'
+
+class CompaniesDetailView(TemplateView):
+    template_name = 'admin_panel/company-detail.html'
