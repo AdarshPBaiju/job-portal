4070556fc58c40eeee1905223c45c2090865c680
Adarsh P Baiju <adarshpbaiju2060@gmail.com>
2024-06-18 17:14:29 +0530

    changed template folder structure

--- job_portal/admin_panel/views.py
+++ job_portal/admin_panel/views.py
@@ -9,52 +9,55 @@ class LoginView(TemplateView):
     template_name = 'admin_panel/login.html'
 
 class ProfileView(TemplateView):
-    template_name = 'admin_panel/profile.html'
+    template_name = 'admin_panel/pages/profile/profile.html'
 
 class ProfileEditView(TemplateView):
-    template_name = 'admin_panel/edit-profile.html'
+    template_name = 'admin_panel/pages/profile/edit-profile.html'
 
 class UsersListView(TemplateView):
-    template_name = 'admin_panel/userslist.html'
+    template_name = 'admin_panel/pages/user/userslist.html'
 
 class UserUpdateView(TemplateView):
-    template_name = 'admin_panel/edit-user.html'
+    template_name = 'admin_panel/pages/user/edit-user.html'
 
 class UserCreateView(TemplateView):
-    template_name = 'admin_panel/add-user.html'
+    template_name = 'admin_panel/pages/user/add-user.html'
 
 class UserDetailView(TemplateView):
-    template_name = 'admin_panel/User-detail.html'
+    template_name = 'admin_panel/pages/user/user-detail.html'
 
 class CompaniesListView(TemplateView):
-    template_name = 'admin_panel/companieslist.html'
+    template_name = 'admin_panel/pages/company/companieslist.html'
 
 class CompanyAddView(TemplateView):
-    template_name = 'admin_panel/add-company.html'
+    template_name = 'admin_panel/pages/company/add-company.html'
 
 class CompanyUpdateView(TemplateView):
-    template_name = 'admin_panel/update-company.html'
+    template_name = 'admin_panel/pages/company/update-company.html'
 
 class CompanyDetailView(TemplateView):
-    template_name = 'admin_panel/company-detail.html'
+    template_name = 'admin_panel/pages/company/company-detail.html'
 
 class CompanyDetailInterviewListView(TemplateView):
-    template_name = 'admin_panel/company-detail-interview.html'
+    template_name = 'admin_panel/pages/company/company-detail-interview.html'
 
 class CompanyDetailApplicantListView(TemplateView):
-    template_name = 'admin_panel/company-detail-applicant-list.html'
+    template_name = 'admin_panel/pages/company/company-detail-applicant-list.html'
 
 class JobListView(TemplateView):
-    template_name = 'admin_panel/job-list.html'
+    template_name = 'admin_panel/pages/job/job-list.html'
 
 class JobDetailView(TemplateView):
-    template_name = 'admin_panel/job-detail.html'
+    template_name = 'admin_panel/pages/job/job-detail.html'
 
 class JobDetailInterviewListView(TemplateView):
-    template_name = 'admin_panel/job-detail-schedule.html'
+    template_name = 'admin_panel/pages/job/job-detail-schedule.html'
 
 class JobCreateView(TemplateView):
-    template_name = 'admin_panel/add-job.html'
+    template_name = 'admin_panel/pages/job/add-job.html'
 
 class JobUpdateView(TemplateView):
-    template_name = 'admin_panel/job-edit.html'
+    template_name = 'admin_panel/pages/job/job-edit.html'
+
+class Test(TemplateView):
+    template_name = 'admin_panel/arrange.html'
