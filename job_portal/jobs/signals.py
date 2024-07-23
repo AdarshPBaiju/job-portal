fdd881eb2c88788418f1132d0efa4b1755970e5b
Adarsh P Baiju <adarshpbaiju2060@gmail.com>
2024-07-23 11:33:21 +0530

    "Refactored signal handlers for job and job application notifications"

--- job_portal/jobs/signals.py
+++ job_portal/jobs/signals.py
@@ -2,7 +2,6 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Job, JobApplication, Notification, NotificationList, JobPortalProfile
 
-
 def create_notification(subject, content, job=None, job_application=None):
     notification, created = Notification.objects.get_or_create(
         job=job,
@@ -14,64 +13,48 @@ def create_notification(subject, content, job=None, job_application=None):
     )
     return notification, created
 
-class JobSignals:
-    @staticmethod
-    @receiver(post_save, sender=Job)
-    def create_job_notifications(sender, instance, created, **kwargs):
-        if created:
-            subject = f"New application received for {instance.job.job_title}"
-            content = f"A new application has been received for the position of {instance.job.job_title} in {instance.job.location}."
-            notification, _ = create_notification(subject, content, job_application=instance)
-            
-            try:
-                employee = JobPortalProfile.objects.get(job=instance.job, job_profile='Employee')
-                NotificationList.objects.get_or_create(
-                    user=employee,
-                    notification=notification
-                )
-            except JobPortalProfile.DoesNotExist:
-                pass
-
-class ApplicationSignals:
-    @staticmethod
-    @receiver(post_save, sender=JobApplication)
-    def create_application_notifications(sender, instance, created, **kwargs):
-        if created:
-            subject = f"New application received for {instance.job.job_title}"
-            content = f"A new application has been received for the position of {instance.job.job_title} in {instance.job.location}."
-            notification, _ = create_notification(subject, content, job_application=instance)
-            
-            try:
-                employee = JobPortalProfile.objects.get(job=instance.job, job_profile='Employee')
-                NotificationList.objects.get_or_create(
-                    user=employee,
-                    notification=notification
-                )
-            except JobPortalProfile.DoesNotExist:
-                pass
-
-
-class ApplicationStatusChangeSignals:
-    @staticmethod
-    @receiver(post_save, sender=JobApplication)
-    def update_application_status_notifications(sender, instance, created, **kwargs):
-        if not created and instance.status in ['Selected', 'Rejected']:
-            subject = f"Application status changed for {instance.job.job_title}"
-            content = f"The application status for {instance.job.job_title} in {instance.job.location} has been changed to {instance.status}."
-            
-            notification = Notification.objects.create(
-                job=None,
-                job_application=instance,
-                subject=subject,
-                content=content
+@receiver(post_save, sender=Job)
+def create_job_notifications(sender, instance, created, **kwargs):
+    if created:
+        subject = f"New Job Opportunity: {instance.job_title.title}"
+        content = f"A new job for {instance.job_title.title} in {instance.location} is now available."
+        notification, _ = create_notification(subject, content, job=instance)
+        
+        jobseekers = JobPortalProfile.objects.filter(title=instance.job_title, job_profile='Job Seeker').exclude(user=instance.user)
+        for jobseeker in jobseekers:
+            NotificationList.objects.get_or_create(
+                user=jobseeker.user,
+                notification=notification
             )
-            
-            # Assign this notification to the job seeker (applicant) who applied for the job
-            try:
-                job_seeker = instance.applicant
-                NotificationList.objects.create(
-                    user=job_seeker,
-                    notification=notification
-                )
-            except JobPortalProfile.DoesNotExist:
-                pass
+
+@receiver(post_save, sender=JobApplication)
+def create_application_notifications(sender, instance, created, **kwargs):
+    if created:
+        subject = f"New Application: {instance.job.job_title}"
+        content = f"A new application for {instance.job.job_title} in {instance.job.location} has been received."
+        notification, _ = create_notification(subject, content, job_application=instance)
+        
+        employee = instance.job.user
+        NotificationList.objects.get_or_create(
+            user=employee,
+            notification=notification
+        )
+
+@receiver(post_save, sender=JobApplication)
+def update_application_status_notifications(sender, instance, created, **kwargs):
+    if not created and instance.status in ['Selected', 'Rejected']:
+        subject = f"Application Status Update: {instance.job.job_title}"
+        content = f"Your application status for {instance.job.job_title} in {instance.job.location} has been updated to {instance.status}."
+        
+        notification = Notification.objects.create(
+            job=None,
+            job_application=instance,
+            subject=subject,
+            content=content
+        )
+        
+        job_seeker = instance.applicant
+        NotificationList.objects.create(
+            user=job_seeker,
+            notification=notification
+        )
